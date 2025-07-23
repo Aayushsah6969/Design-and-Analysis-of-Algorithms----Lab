2fbf079c3d6456f446d6dcda814c58f84cdd5114
Aayushsah6969 <aayushsah269@gmail.com>
2025-07-23 22:24:20 +0530

    lab-1 complete

--- Lab_1/4/4.c
+++ Lab_1/4/4.c
@@ -1,1 +1,58 @@
-new chnage
+#include <stdio.h>
+#include <stdlib.h>
+
+void EXCHANGE(int *p, int *q) {
+    int temp = *p;
+    *p = *q;
+    *q = temp;
+}
+
+void ROTATE_RIGHT(int *arr, int p2) {
+    int i;
+    for (i = p2 - 1; i > 0; i--) {
+        EXCHANGE(&arr[i], &arr[i-1]);
+    }
+}
+
+int main() {
+    FILE *f_in, *f_out;
+    int arr[100];
+    int n = 0, num, i;
+    int p2 = 5; // Number of elements to rotate
+
+    // Read array from input.txt
+    f_in = fopen("input.txt", "r");
+    if (f_in == NULL) {
+        printf("Error opening input.txt\n");
+        return 1;
+    }
+    while (fscanf(f_in, "%d", &num) == 1) {
+        arr[n++] = num;
+    }
+    fclose(f_in);
+
+    // Write before rotation to output.txt
+    f_out = fopen("output.txt", "w");
+    if (f_out == NULL) {
+        printf("Error opening output.txt\n");
+        return 1;
+    }
+    fprintf(f_out, "Before ROTATE: ");
+    for (i = 0; i < n; i++) {
+        fprintf(f_out, "%d ", arr[i]);
+    }
+    fprintf(f_out, "\n");
+
+    // Rotate first p2 elements
+    ROTATE_RIGHT(arr, p2);
+
+    // Write after rotation to output.txt
+    fprintf(f_out, "After ROTATE: ");
+    for (i = 0; i < n; i++) {
+        fprintf(f_out, "%d ", arr[i]);
+    }
+    fprintf(f_out, "\n");
+    fclose(f_out);
+
+    return 0;
+}
